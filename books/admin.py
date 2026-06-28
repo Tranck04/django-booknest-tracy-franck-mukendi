cdcd00d80466535ddce689835894d73e4a0d4756
Tracy Franck Mukendi <tracymukendi017@gmail.com>
2026-06-28 20:17:22 +0100

    Phase 3 - Configuration de Django Admin et peuplement des donnees de test

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Category, Book, UserBook
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'category', 'added_to_catalog')
+    list_filter = ('category',)
+    search_fields = ('title', 'author', 'isbn')
+    ordering = ('-added_to_catalog',)
+    date_hierarchy = 'added_to_catalog'
+
+
+@admin.register(UserBook)
+class UserBookAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'reading_status', 'is_favorite', 'added_date')
+    list_filter = ('reading_status', 'is_favorite', 'is_read_later')
+    search_fields = ('user__username', 'book__title')
+    ordering = ('-added_date',)
